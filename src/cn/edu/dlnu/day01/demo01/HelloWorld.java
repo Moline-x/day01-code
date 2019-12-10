8235a77c7fa16e0cd09bf427c7b98cb91774a373
Moline-x <2223745240@qq.com>
2019-12-10 10:54:33 +0800

    基础代码101第一次提交

--- src/cn/edu/dlnu/day01/demo01/HelloWorld.java
+++ src/cn/edu/dlnu/day01/demo01/HelloWorld.java
@@ -3,7 +3,7 @@ package cn.edu.dlnu.day01.demo01;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello,My World!");
+        System.out.println("Hello, World!");
         
     }
 }
